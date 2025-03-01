891516100bca83dbdf7e3cc719df0bc05196b609
Gabriel Guimarães <gabsvander@hotmail.com>
2025-02-28 21:03:48 -0300

    FROST-20 Add i18n support

--- features/cross_platform/flutter_application/lib/main.dart
+++ features/cross_platform/flutter_application/lib/main.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/src/presentation/glue/frb_generated.dart';
 import 'package:flutter_application/src/presentation/pages/main_page.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 Future<void> main() async {
   await RustLib.init();
-  runApp(const MyApp());
+
+  final FlutterI18nDelegate flutterI18nDelegate = FlutterI18nDelegate(
+    translationLoader: FileTranslationLoader(useCountryCode: true, basePath: 'assets/i18n', fallbackFile: 'en_us.yaml'),
+    missingTranslationHandler: (key, locale) => print("[${locale}] Key not found: $key"),
+  );
+
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp(flutterI18nDelegate));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp(this.flutterI18nDelegate, {super.key});
+  final FlutterI18nDelegate flutterI18nDelegate;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MainPage());
+    return MaterialApp(
+      localizationsDelegates: [
+        flutterI18nDelegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      builder: FlutterI18n.rootAppBuilder(),
+      home: const MainPage(),
+    );
   }
 }
