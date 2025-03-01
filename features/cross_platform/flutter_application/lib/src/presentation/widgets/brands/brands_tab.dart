575dea109f968f6153bd1e365f023bd24cc6f78d
Gabriel Guimarães <gabsvander@hotmail.com>
2025-02-28 21:56:15 -0300

    FROST-19 Modularize brands tab

--- features/cross_platform/flutter_application/lib/src/presentation/widgets/brands/brands_tab.dart
+++ features/cross_platform/flutter_application/lib/src/presentation/widgets/brands/brands_tab.dart
@@ -10,48 +10,64 @@ class BrandsTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(
-          child: ListView.separated(
-            itemCount: brands.length,
-            itemBuilder: (context, index) => Card(child: ListTile(title: Text(brands[index]))),
-            separatorBuilder: (context, _) => const SizedBox(height: 10),
-          ),
+        Expanded(child: BrandListDisplay(brands: brands)),
+        const Row(mainAxisAlignment: MainAxisAlignment.center, children: [AddNewBrandButton()]),
+      ],
+    );
+  }
+}
+
+class AddNewBrandButton extends StatelessWidget {
+  const AddNewBrandButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () => showDialog<void>(context: context, builder: (BuildContext context) => const NewBrandDialog()),
+      icon: const Icon(Icons.add),
+      iconSize: 60,
+      color: Theme.of(context).primaryColor,
+    );
+  }
+}
+
+class NewBrandDialog extends StatelessWidget {
+  const NewBrandDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(context.i18n('brands_tab.new_brand_dialog.title')),
+      content: Container(),
+      actions: <Widget>[
+        TextButton(
+          child: Text(context.i18n('brands_tab.new_brand_dialog.cancel_button')),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              onPressed:
-                  () => showDialog<void>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text(context.i18n('brands_tab.new_brand_dialog.title')),
-                        content: Container(),
-                        actions: <Widget>[
-                          TextButton(
-                            child: Text(context.i18n('brands_tab.new_brand_dialog.cancel_button')),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          ElevatedButton(
-                            child: Text(context.i18n('brands_tab.new_brand_dialog.save_button')),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-              icon: const Icon(Icons.add),
-              iconSize: 60,
-              color: Theme.of(context).primaryColor,
-            ),
-          ],
+        ElevatedButton(
+          child: Text(context.i18n('brands_tab.new_brand_dialog.save_button')),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
         ),
       ],
+    );
+  }
+}
+
+class BrandListDisplay extends StatelessWidget {
+  const BrandListDisplay({required this.brands, super.key});
+
+  final List<String> brands;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: brands.length,
+      itemBuilder: (_, index) => Card(child: ListTile(title: Text(brands[index]))),
+      separatorBuilder: (_, _) => const SizedBox(height: 10),
     );
   }
 }
