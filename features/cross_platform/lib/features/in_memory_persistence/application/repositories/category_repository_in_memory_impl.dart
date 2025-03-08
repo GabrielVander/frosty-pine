52f45b59f161a5613bcf173a9a93a9b180609e71
Gabriel Guimarães <gabsvander@hotmail.com>
2025-03-08 16:28:33 -0300

    FROST-27 Add single category fetching in-memory implementation

---
+++ features/cross_platform/lib/features/in_memory_persistence/application/repositories/category_repository_in_memory_impl.dart
@@ -0,0 +1,22 @@
+import 'package:frosy_pine/features/core/domain/entities/category.dart';
+import 'package:frosy_pine/features/core/domain/repositories/category_repository.dart';
+import 'package:rust/rust.dart';
+
+class CategoryRepositoryInMemoryImpl implements CategoryRepository {
+  CategoryRepositoryInMemoryImpl({required this.data});
+
+  final Map<String, String> data;
+
+  @override
+  Future<Result<Category, CategoryRepositoryFailure>> fetchSingleById(String id) async {
+    final String? brandName = data[id];
+
+    if (brandName == null) {
+      return Err<Category, CategoryRepositoryFailure>(
+        CategoryRepositoryNotFoundFailure(message: 'Category not found', details: 'Unable to find category with id $id'),
+      );
+    }
+
+    return Ok<Category, CategoryRepositoryFailure>(Category(id: id, name: brandName));
+  }
+}
