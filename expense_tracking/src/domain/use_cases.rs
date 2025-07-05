068412fe9b3d199f5c9ff1b94700bb7d182975a7
Gabriel Guimarães <prism109@duck.com>
2025-07-05 00:04:10 -0300

    Flatten feature folders

--- expense_tracking/src/domain/use_cases.rs
+++ expense_tracking/src/domain/use_cases.rs
@@ -1,8 +1,6 @@
 mod add_new_brand;
 mod retrieve_all_brands_use_case;
-mod use_case_input_port;
 
 pub use add_new_brand::{AddNewBrandInteractor, AddNewBrandInteractorError, AddNewBrandOutputPort};
 pub use retrieve_all_brands_use_case::RetrieveAllBrandsUseCase;
 pub use retrieve_all_brands_use_case::RetrieveAllBrandsUseCaseError;
-pub use use_case_input_port::UseCaseInputPort;
